15acb1dfcf2c6745fc0137af1c61e50f788bec9b
cschweda <cschweda@gmail.com>
2021-03-22 10:10:36 -0500

    fix: add remaining ie11 pages

--- components/global/SkipLinks.vue
+++ components/global/SkipLinks.vue
@@ -3,6 +3,10 @@
     <a id="skip-main" href="#main" aria-label="Skip Link">
       Skip to Main Content
     </a>
+    |
+    <a id="skip-ie11" href="/irb/ie11/" aria-label="Skip to IE11 friendly">
+      Skip to Internet Explorer friendly site
+    </a>
   </div>
 </template>
 
